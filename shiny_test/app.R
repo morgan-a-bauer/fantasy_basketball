240339e28cb86d224025daef6e602c5031495cef
Morgan Bauer <morgan.a.bauer@gmail.com>
2023-07-05 16:53:07 -0400

    Update app.R
    Started working on test app.R

--- shiny_test/app.R
+++ shiny_test/app.R
@@ -1,1 +1,23 @@
+library(shiny)
 
+# Define UI
+ui <- fluidPage(
+
+    # App title
+    titlePanel("Test UI")
+
+    # Sidebar layout
+    sidebarLayout(
+
+        # Sidebar panels for inputs
+        sidebarPanel(
+
+            #Input slider
+            sliderInput(inputId = "things",
+                        label = "Number of things",
+                        min = 1,
+                        maax = 50,
+                        value = 30)
+        ),
+    )
+)
