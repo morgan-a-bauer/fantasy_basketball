40e8ebbc137c8e6af6221d00007c3d1ff939d74e
Morgan Bauer <morgan.a.bauer@gmail.com>
2023-08-13 14:35:41 -0400

    Added a function to deal with player stat input and commented some stuff

--- analytics_app/app.R
+++ analytics_app/app.R
@@ -52,7 +52,7 @@ ui <- navbarPage("The Common Denominator",
                     )
                 ),
                 mainPanel(
-                    plotOutput("comparison_plot")
+
                 )
             )
         ),
@@ -67,30 +67,35 @@ server <- function(input, output) {
 
     # Compare Players Page
     observe({
+        # Initialize a new reactiveValues object
         comparison_values <- reactiveValues()
         comparison_values$df <- data.frame()
 
+        # Get input from search bar
         selected_players <- input$player_comparison_search
 
         if (!is.null(selected_players) && length(selected_players) > 0) {
 
             comparison_values$df <- data.frame()
 
+            # Gather game logs from Basketball Reference and prepare comparison output
             for (new_player in selected_players) {
 
-                print(new_player)
                 new_id <- subset(nba_players, name == new_player, select = id_code)
 
                 if (length(new_id) > 0) {
 
-                    print(new_id)
                     first_letter <- new_id[1]
                     game_log_url <- sprintf("https://www.basketball-reference.com/players/%s/%s/gamelog/2023", first_letter, new_id)
+
+                    # We-scraping the game logs
                     isolate({
 
                         tables <- game_log_url %>%
                             read_html() %>%
                             html_nodes("table")
+
+                        # Modifying data table to only care about FAN_PTS for each individual player
                         new_table <- html_table(tables[[length(tables)]], header = TRUE) %>%
                             select(Rk, TRB, AST, STL, BLK, TOV, PTS) %>%
                             mutate(Player = new_player) %>%
@@ -103,12 +108,14 @@ server <- function(input, output) {
                             mutate(FAN_PTS = PTS + (3 * BLK) + (3 * STL) - TOV + (1.5 * AST) + (1.2 * TRB)) %>%
                             select(Player, Game, FAN_PTS)
 
+                        # Combining and modifying data tables from all players for both the plot and the table
                         comparison_values$df <- bind_rows(comparison_values$df, new_table)
                         comparison_plot_table <- comparison_values$df %>% replace(is.na(.), 0) %>%
                             mutate(Game = as.integer(Game))
                         wide_comparison_table <- comparison_values$df %>%
                             pivot_wider(names_from = "Game", values_from = "FAN_PTS")
 
+            # Plot Output
             output$comparison_plot <- renderPlot(
                 ggplot(data = comparison_plot_table) +
                     geom_line(mapping = aes(x = Game, y = FAN_PTS, color = Player)) +
@@ -117,9 +124,67 @@ server <- function(input, output) {
                     ylab("Fantasy Points") +
                     xlab("Game Number")
             )
+
+            # Data Table Output
             output$comparison_table <- renderDataTable(wide_comparison_table)
                     })
                 }
+            }
+        }
+    })
+
+    # Player Stats Page
+    observe({
+        player_stats_values <- reactiveValues()
+        player_stats_values$df <- data.frame()
+
+        selected_player <- input$player_stat_search
+
+        if (!is.null(selected_players) && length(selected_players) > 0) {
+
+            player_stats_values$df <- data.frame()
+
+            print(new_player)
+            new_id <- subset(nba_players, name == new_player, select = id_code)
+
+            if (length(new_id) > 0) {
+
+                print(new_id)
+                first_letter <- new_id[1]
+                game_log_url <- sprintf("https://www.basketball-reference.com/players/%s/%s/gamelog/2023", first_letter, new_id)
+                isolate({
+
+                    tables <- game_log_url %>%
+                        read_html() %>%
+                        html_nodes("table")
+                    new_table <- html_table(tables[[length(tables)]], header = TRUE) %>%
+                        select(Rk, TRB, AST, STL, BLK, TOV, PTS) %>%
+                        mutate(Player = new_player) %>%
+                        filter(Rk != "Rk") %>%
+                        select(Rk, TRB, AST, STL, BLK, TOV, PTS, Player) %>%
+                        mutate(TRB = as.integer(TRB), AST = as.integer(AST),
+                               STL = as.integer(STL), BLK = as.integer(BLK),
+                               TOV = as.integer(TOV), PTS = as.integer(PTS)) %>%
+                        rename(Game = 'Rk') %>%
+                        mutate(FAN_PTS = PTS + (3 * BLK) + (3 * STL) - TOV + (1.5 * AST) + (1.2 * TRB)) %>%
+                        select(Player, Game, FAN_PTS)
+
+                    comparison_values$df <- bind_rows(comparison_values$df, new_table)
+                    comparison_plot_table <- comparison_values$df %>% replace(is.na(.), 0) %>%
+                        mutate(Game = as.integer(Game))
+                    wide_comparison_table <- comparison_values$df %>%
+                        pivot_wider(names_from = "Game", values_from = "FAN_PTS")
+
+                    output$comparison_plot <- renderPlot(
+                        ggplot(data = comparison_plot_table) +
+                            geom_line(mapping = aes(x = Game, y = FAN_PTS, color = Player)) +
+                            scale_fill_brewer(palette = "Spectral") +
+                            labs(title = "Fantasy Points for each Game") +
+                            ylab("Fantasy Points") +
+                            xlab("Game Number")
+                    )
+                    output$comparison_table <- renderDataTable(wide_comparison_table)
+                })
             }
         }
     })
