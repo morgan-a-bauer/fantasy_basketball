056ea13d15069cd68c93eab4d51962527fce93d0
Morgan Bauer <131829467+morgan-a-bauer@users.noreply.github.com>
2025-01-21 14:07:17 -0500

    Firsst iOS test app

---
+++ Landmarks/Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Morgan Bauer on 1/21/25.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
